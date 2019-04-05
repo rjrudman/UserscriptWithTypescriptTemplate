149044c170336e0dc4542c5d90ced99345f72461
Robert Rudman <rjrudman@gmail.com>
2019-04-05 14:34:25 +1100

    Add reference to require.js

--- src/UserScriptDeclaration.js
+++ src/UserScriptDeclaration.js
@@ -1,4 +1,5 @@
 // ==UserScript==
 // @name         Demo Script
 // @match        *://*.stackoverflow.com/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js
 // ==/UserScript==
